43a7ec97165b2b2370319c085bc3ef78fdecdac8
burek man <Alnu07001@utb.vaxjo.se>
2024-08-28 11:16:46 +0200

    namn efternamn intro uppgift

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using System.Xml;
+
+Console.WriteLine("namn och efternamn");
+string text = Console.ReadLine();
+string text2 = Console.ReadLine();
+Console.Write(text + " " + text2);
